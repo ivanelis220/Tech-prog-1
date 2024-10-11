05c7ab5781ff6f85f8a239c9e28d4ffd1e8398af
ivanelis <ivanelis404@gmail.com>
2024-10-11 23:15:25 +0300

    Создать проект src

---
+++ src/Keeper.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "Tray.h"
+#include "Sail.h"
+#include "Boat.h"
+#include <fstream>
+
+class Keeper
+{
+	Ship** data;
+	int size;
+public:
+	Keeper();
+	~Keeper();
+	int getSize();
+	void add();
+	void add(int _type, ifstream& fin);
+	void edit();
+	void del();
+	void save();
+	void load();
+	friend ostream& operator<<(ostream& out, Keeper& obj);
+};
---
+++ src/ship.cpp
@@ -0,0 +1,25 @@
+#include "ship.h"
+
+void Ship::setType(int _type)
+{
+	type = _type;
+}
+
+int Ship::getType()
+{
+	return type;
+}
+
+bool Ship::isError()
+{
+	return this->error;
+}
+
+void Ship::setError(bool _error)
+{
+	this->error = _error;
+}
+
+Ship::~Ship()
+{
+}
